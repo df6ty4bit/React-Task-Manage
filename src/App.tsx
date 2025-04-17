1518bbe2180bd6fa98437c38cd63ee10145a304b
df6ty4bit <stephengodwin895@gmail.com>
2025-04-17 08:02:22 +0100

    second Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TaskHandler from "./components/TaskHandler"
+
+const App = () => {
+    return (
+        <div>
+            <TaskHandler />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/TaskHandler.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import "./TaskHandler.css";
+import { useState } from "react";
+
+const TaskHandler = () => {
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+    const [taskUpdater, setTaskUpdater] = useState("");
+    return (
+        <>
+            <h1 className="header">Task Manager App</h1>
+            <form className="new-item-form">
+                <div className="form-row">
+                    <input
+                        placeholder="Enter New Task"
+                        type="text"
+                        name="task"
+                        className="input"
+                        value={taskUpdater}
+                        onChange={e => {
+                            setTaskUpdater(e.target.value);
+                        }}
+                    />
+                    <button className="btn" onSubmit={handleSubmit}>
+                        Add Task
+                    </button>
+                </div>
+                <h3>Added Task</h3>
+                <ul className="list">
+                    <li>
+                        <label for="taskitem">
+                            <input
+                                type="checkbox"
+                                name="taskitem"
+                                className="input"
+                            />
+                            Item 1
+                        </label>
+                        <button className="btn btn-danger">Delete</button>
+                    </li>
+                    <li>
+                        <label for="taskitem">
+                            <input
+                                type="checkbox"
+                                name="taskitem"
+                                className="input"
+                            />
+                            Item 1
+                        </label>
+                        <button className="btn btn-danger">Delete</button>
+                    </li>
+                </ul>
+            </form>
+        </>
+    );
+};
+
+export default TaskHandler;
